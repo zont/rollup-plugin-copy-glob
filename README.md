9b50fccb9498eeb7bcac101050265e8e8b14ad09
alexander.zonov <alexander.zonov@developonbo.ru>
2018-05-03 18:39:01 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# rollup-plugin-copy-glob
+
+Rollup plugin to easily copy files and folders. Glob supported
+
+## Install
+
+```sh
+npm i -D rollup-plugin-copy-glob
+```
+
+## Usage
+
+```js
+import copy from 'rollup-plugin-copy-glob';
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife',
+    name: 'app'
+  },
+  plugins: [
+    copy([
+      { files: 'src/*.{html,css}', dest: 'dist' },
+      { files: 'dev/fonts/*.*', dest: 'dist/fonts' },
+      { files: 'dev/images/**/*.*', dest: 'dist/images' }
+    ])
+  ]
+};
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+const { promisify } = require('util');
+const glob = require('glob');
+const globParent = require('glob-parent');
+const { name } = require('./package.json');
+
+require('colors');
+
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+const createDirIfNotExist = to => {
+  const dirs = [];
+  let dir = path.dirname(to);
+
+  while (dir !== path.dirname(dir)) {
+    dirs.unshift(dir);
+    dir = path.dirname(dir);
+  }
+
+  dirs.forEach(dir => {
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir);
+    }
+  });
+};
+
+const copy = async (from, entry) => {
+  const to = path.join(entry.dest, path.relative(globParent(entry.files), from));
+
+  createDirIfNotExist(to);
+
+  if (!fs.statSync(from).isDirectory()) {
+    try {
+      await writeFileAsync(to, await readFileAsync(from));
+
+      console.log('[COPY]'.yellow, from, 'to'.yellow, to);
+    } catch (e) {
+      console.log('[COPY][ERROR]'.red, from);
+      console.error(e);
+    }
+  }
+};
+
+export default paths => {
+  return {
+    name,
+    ongenerate() {
+      for (const entry of paths) {
+        glob.sync(entry.files).forEach(file => copy(file, entry));
+      }
+    }
+  };
+};
